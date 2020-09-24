460c03b6ca7fb3df5a5d8c5136a78e67ca1436af
Crowdbotics <team@crowdbotics.com>
2020-09-24 00:13:54 +0000

    Update models

--- backend/wellness_rx_20621/wsgi.py
+++ backend/wellness_rx_20621/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wellness_rx_20621.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wellness_rx_20621.settings")
 
 application = get_wsgi_application()
